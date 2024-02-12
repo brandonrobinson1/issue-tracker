bcb0cd12d93e760ca20f46c1c8866d7b0d73cdf3
brobinson21 <brandonrobinson0000@gmail.com>
2024-02-12 14:35:07 -0500

    add navbar

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
+import { FaBug } from "react-icons/fa";
+
 const NavBar = () => {
+    const links = [{ label: 'Dashboard', href: '/' },
+    {label:"Issues", href:'/issues'}]
+
   return (
       <nav className="flex space-x-6 border-b mb-5 px-5 h-14 items-center">
-          <Link href={"/"}>Logo</Link>
+          <Link href={"/"}><FaBug/></Link>
           <ul className='flex space-x-6'>
-         <li><Link href='/'></Link>Dashboard</li> 
-            <li>  <Link href='/issues'>Issues</Link></li>
+              {links.map(link => <Link className='text-zinc-500 hover:text-zinc-800 transition-colors' href={link.href}>{link.label}</Link>)}
+         
               </ul>
     </nav>
   )
 }
-
+ 
 export default NavBar
