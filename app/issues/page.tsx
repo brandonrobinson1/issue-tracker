272fcf813b8f3989a3ce33b05a02f91a1fe7b2cf
brobinson21 <brandonrobinson0000@gmail.com>
2024-02-16 16:37:40 -0500

    table update

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import {Button} from '@radix-ui/themes'
-import Link from 'next/link'
+import React from "react";
+import { Button, Table, TableColumnHeaderCell } from "@radix-ui/themes";
+import Link from "next/link";
+import prisma from "@/prisma/client";
 const IssuesPage = () => {
+  const issues = prisma.issue.findMany();
+
   return (
     <div>
-      <Button><Link href='/issues/new'>New Issue</Link></Button>
+      <Button>
+        <Link href="/issues/new">New Issue</Link>
+      </Button>
+      <Table.Root>
+        <Table.Header>
+          <Table.Body>
+            <Table.Row>
+              <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
+              <TableColumnHeaderCell>Status</TableColumnHeaderCell>
+              <TableColumnHeaderCell>Data</TableColumnHeaderCell>
+            </Table.Row>
+          </Table.Body>
+        </Table.Header>
+      </Table.Root>
     </div>
-  )
-}
+  );
+};
 
-export default IssuesPage
+export default IssuesPage;
