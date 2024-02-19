1476e0fae6a9f959a70418165666b5f8b524efd9
brobinson21 <brandonrobinson0000@gmail.com>
2024-02-19 14:28:40 -0500

    build issue status badge

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,24 +1,42 @@
 import React from "react";
-import { Button, Table, TableColumnHeaderCell } from "@radix-ui/themes";
+import {
+  Button,
+  Table,
+  TableColumnHeaderCell,
+  TableRow,
+} from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
-const IssuesPage = () => {
-  const issues = prisma.issue.findMany();
+import IssuesStatusBadge from "../components/IssuesStatusBadge";
+const IssuesPage = async () => {
+  const issues = await prisma.issue.findMany();
 
   return (
     <div>
-      <Button>
-        <Link href="/issues/new">New Issue</Link>
-      </Button>
-      <Table.Root>
+      <div className="mb-5">
+        <Button>
+          <Link href="/issues/new">New Issue</Link>
+        </Button>
+      </div>
+      <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
             <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
             <TableColumnHeaderCell>Status</TableColumnHeaderCell>
             <TableColumnHeaderCell>Data</TableColumnHeaderCell>
           </Table.Row>
-          <Table.Body>{issues}</Table.Body>
         </Table.Header>
+        <Table.Body>
+          {issues.map((issue) => (
+            <TableRow key={issue.id}>
+              <Table.Cell>{issue.title}</Table.Cell>
+              <Table.Cell>
+                <IssuesStatusBadge status={issue.status} />
+              </Table.Cell>
+              <Table.Cell>{issue.createdAt.toDateString()}</Table.Cell>
+            </TableRow>
+          ))}
+        </Table.Body>
       </Table.Root>
     </div>
   );
