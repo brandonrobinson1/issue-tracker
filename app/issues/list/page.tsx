4522531b85d3793cc1536a7a866c0dd883c1c474
brobinson21 <brandonrobinson0000@gmail.com>
2024-03-08 13:50:13 -0500

    filter issues

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -3,9 +3,21 @@ import { Table, TableColumnHeaderCell, TableRow } from "@radix-ui/themes";
 import { IssuesStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
 import { useRouter } from "next/navigation";
+import { Status } from "@prisma/client";
 
-const IssuesPage = async () => {
-  const issues = await prisma.issue.findMany();
+const IssuesPage = async ({
+  searchParams,
+}: {
+  searchParams: { status: Status };
+}) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+
+  const issues = await prisma.issue.findMany({
+    where: { status },
+  });
   return (
     <div>
       <IssueActions />
