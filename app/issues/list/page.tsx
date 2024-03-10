e97a1dc0ed3ac8764c65a8ef2527016dd206a7e5
brobinson21 <brandonrobinson0000@gmail.com>
2024-03-09 20:30:03 -0500

    implement pagination

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -2,21 +2,33 @@ import prisma from "@/prisma/client";
 import { Table, TableColumnHeaderCell, TableRow } from "@radix-ui/themes";
 import { IssuesStatusBadge, Link } from "@/app/components";
 import IssueActions from "./IssueActions";
-import { useRouter } from "next/navigation";
-import { Status } from "@prisma/client";
+import NextLink from "next/link";
+import { Issue, Status } from "@prisma/client";
+import { ArrowUpIcon } from "@radix-ui/react-icons";
 
 const IssuesPage = async ({
   searchParams,
 }: {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
+  const columns: { label: string; value: keyof Issue; className?: string }[] = [
+    { label: "Issue", value: "title" },
+    { label: "Status", value: "status", className: "hidden md:table-cell" },
+    { label: "Created", value: "createdAt", className: "hidden md:table-cell" },
+  ];
   const statuses = Object.values(Status);
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
 
+  const orderBy = columns
+    .map((column) => column.value)
+    .includes(searchParams.orderBy)
+    ? { [searchParams.orderBy]: "asc" }
+    : undefined;
   const issues = await prisma.issue.findMany({
     where: { status },
+    orderBy,
   });
   return (
     <div>
@@ -24,13 +36,21 @@ const IssuesPage = async ({
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <TableColumnHeaderCell>Issue</TableColumnHeaderCell>
-            <TableColumnHeaderCell className="hidden md:table-cell">
-              Status
-            </TableColumnHeaderCell>
-            <TableColumnHeaderCell className="hidden md:table-cell">
-              Data
-            </TableColumnHeaderCell>
+            {columns.map((column) => (
+              <TableColumnHeaderCell
+                className={column.className}
+                key={column.value}
+              >
+                <NextLink
+                  href={{ query: { ...searchParams, orderBy: column.value } }}
+                >
+                  {column.label}
+                </NextLink>
+                {column.value === searchParams.orderBy && (
+                  <ArrowUpIcon className="inline" />
+                )}
+              </TableColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
         <Table.Body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Pagination from "./components/Pagination";
 
 export default function Home() {
-  return (
-    <main >
-     hello world
-    </main>
-  )
+  return <Pagination itemCount={100} pageSize={10} currentPage={2} />;
 }
