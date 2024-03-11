9a6de02412d8be99221150f47206e36e7001b425
brobinson21 <brandonrobinson0000@gmail.com>
2024-03-11 18:13:01 -0400

    add metadata

--- app/issues/list/page.tsx
+++ app/issues/list/page.tsx
@@ -5,6 +5,7 @@ import Pagination from "@/app/components/Pagination";
 import { Status } from "@prisma/client";
 import IssueTable, { IssueQuery, columnNames } from "./IssueTable";
 import { Flex } from "@radix-ui/themes";
+import { Metadata } from "next";
 
 interface Props {
   searchParams: IssueQuery;
@@ -46,5 +47,10 @@ const IssuesPage = async ({ searchParams }: Props) => {
 };
 
 export const dynamic = "force-dynamic";
+
+export const metadata: Metadata = {
+  title: "Issue Tracker - Issue list",
+  description: "View all project issues",
+};
 
 export default IssuesPage;
