c06d465fe0336140d24e4459ec63ff679b410b87
brobinson21 <brandonrobinson0000@gmail.com>
2024-03-28 12:35:56 -0400

    add type

--- app/issues/list/IssueActions.tsx
+++ app/issues/list/IssueActions.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import React from "react";
 import IssueStatusFilter from "./IssueStatusFilter";
 
-const IssueActions = () => {
+const IssueActions: () => React.JSX.Element = () => {
   return (
     <Flex justify="end">
       {/* <IssueStatusFilter /> */}
